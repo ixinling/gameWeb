7a29ef1bfba71d429e36f7660059798bd89d06d7
LiuBing <liubing@insightone.cn>
2024-12-08 20:50:39 +0800

    add

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ export default {
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    target: 'esnext',
+    minify: 'terser'
   },
   server: {
     port: 3000,
     open: true
-  }
+  },
+  base: '/'
 }
